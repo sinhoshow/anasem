d01c0f402b7214dd7fa5e6f7ad4110b540e1f092
Nilson Augusto <nilson.augusto@gerenciagram.com.br>
2020-03-30 16:42:51 -0300

    Abertura e geração do arquivo de saída

--- AnaSem.py
+++ AnaSem.py
@@ -15,4 +15,20 @@ class AnaSem():
         arquivos = os.listdir('input')        
         for arquivo in arquivos:
             arquivo = open('input/' + arquivo, 'r')
-            #codigo
+            erros = []
+            #processar codigo
+
+
+            arquivo.close()
+            conteudo = ''            
+            if (erros):
+                conteudo += '\n\n\nErros: \n\n'
+                for erro in erros:
+                    conteudo += 'Linha:'+ str(erro.linha)+ ' / Lexema:'+ erro.lexema+ ' / Token: '+ erro.erro+ '\n'
+            else:
+                conteudo += "SUCESSO! sem erros"
+            
+            saida = open('output/' + arquivo, 'w')
+            saida.write(conteudo)
+            saida.close()
+            
