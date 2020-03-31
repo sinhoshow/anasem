63b283f045a9007d85788ba17d335f59adfd232a
Nilson Augusto <nilson.augusto@gerenciagram.com.br>
2020-03-30 21:20:41 -0300

    atualizando separacao dos grupos

--- AnaSem.py
+++ AnaSem.py
@@ -18,14 +18,15 @@ class AnaSem():
         arquivos = os.listdir('input')        
         for nome_arquivo in arquivos:
             elementos = analex.analisa(nome_arquivo)
-            groups = self.find_bracket_groups(elementos)
+            elementos_lexemas = self.get_lexemas(elementos)
+            groups = self.find_bracket_groups(elementos_lexemas)
             self.preencher_estruturas(groups[0])
-            self.preencher_typedefs(groups[1])
+            """self.preencher_typedefs(groups[1])
             self.preencher_constantes(groups[2])
             self.preencher_variaveis_globais(groups[3])
             self.preencher_funcoes(groups[4])
             self.preencher_procedimos(groups[5])
-            self.preencher_start(groups[6])
+            self.preencher_start(groups[6])"""
             conteudo = ''
             if (erros):
                 conteudo += '\n\n\nErros: \n\n'
@@ -38,18 +39,26 @@ class AnaSem():
             saida.write(conteudo)
             saida.close()
 
-    def find_bracket_groups(self, elementos):
+    def get_lexemas(self, elementos):
+        lexemas = []
+        for elemento in elementos:
+            lexemas.append(elemento.lexema)
+        return lexemas
+        
+    def find_bracket_groups(self, texts):
         open_brackets = 0
         groups = []
         current_group = []
-        for i, elemento in enumerate(elementos):
-            if elemento.lexema == "{":
+        for i, elemento in enumerate(texts):
+            if elemento == "{":
                 open_brackets += 1
             if open_brackets > 0:
-                current_group.append(elementos[i].lexema)
-            if elemento.lexema == "}":
+                current_group.append(texts[i])
+            if elemento == "}":
                 open_brackets -= 1
                 if open_brackets == 0:
+                    del(current_group[0])
+                    del(current_group[-1])
                     groups.append(current_group)
                     current_group = []
                 if open_brackets < 0:
@@ -57,7 +66,10 @@ class AnaSem():
         return groups
     
     def preencher_estruturas(self, group):
-        print(group)
+        indexes_structs = [i for i, e in enumerate(group) if e == 'struct']
+        contents_strucuts = self.find_bracket_groups(group)
+        print(len(contents_strucuts))
+
     def preencher_typedefs(self, group):
         print(group)
     def preencher_constantes(self, group):
