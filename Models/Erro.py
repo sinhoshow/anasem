a77b5e6daeb5beb5bedf92a00d87c98834a84adb
Khaick Brito <khaickbrito@gmail.com>
2020-03-30 23:54:40 -0300

    Update Erro.py

--- Models/Erro.py
+++ Models/Erro.py
@@ -3,10 +3,10 @@ import os.path
 import string
 
 class Erro():
-    def __init__(self,erro,lexema,linha):
-        self.lexema = lexema
+    def __init__(self,tipo,erro,linha):
+    	self.tipo = tipo
         self.erro = erro
         self.linha = linha
 
     def __str__(self):
-        return self.lexema + '  ' + self.erro
+        return self.tipo + '  ' + self.erro
